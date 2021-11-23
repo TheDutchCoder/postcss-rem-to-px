4a51d942675509963b2b085f66bb1b355e46dd3b
Reinier Kaper <rp.kaper+github@gmail.com>
2021-11-23 11:39:35 -0500

    fix: :bug: Fixes issue where fonts were converted

--- index.test.js
+++ index.test.js
@@ -24,6 +24,10 @@ it('does not add units to 0 values', async () => {
   await run('.m-2 { margin: 0rem 7em; }', '.m-2 { margin: 0 7em; }')
 })
 
+it('does not process non rem values', async () => {
+  await run('.font { font-family: ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"; }', '.font { font-family: ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"; }')
+})
+
 it('considers the baseValue', async () => {
   await run('.m-2 { margin: -0.125rem 7em; }', '.m-2 { margin: -1.25px 7em; }', { baseValue: 10 })
 })
